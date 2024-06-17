9d3a0c005074c179e2b4513d77196667f8e62c32
dejwid <dawid.paszko@gmail.com>
2024-06-17 18:27:01 +0200

    initial commit

---
+++ src/app/api/route.js
@@ -0,0 +1,14 @@
+import {GPTScript} from "@gptscript-ai/gptscript";
+
+export const config = {
+  maxDuration: 60,
+};
+
+export async function GET() {
+  const g = new GPTScript();
+  const run = await g.run('./test.gpt');
+  const result =  await run.text();
+  await run.close();
+  await g.close();
+  return Response.json(result);
+}
